ecabf17080bcc68524fad16eba8fd68cbebaaefa
yago1994 <yagoaral@hotmail.com>
2025-12-31 12:27:45 +0100

    fixed to routing basepath;

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // Use base path for GitHub Pages, fallback to root for local development
-  base: process.env.VITE_BASE_PATH || "/pasalacabra/",
+  base: process.env.VITE_BASE_PATH || "/",
   plugins: [react()],
   server: {
     allowedHosts: ["erma-dogged-edmond.ngrok-free.dev"],
